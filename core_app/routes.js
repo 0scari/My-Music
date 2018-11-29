2e1e42e0154c267668270a1ae7dad8e7e9151652
Oskars Pozdnakovs <opozdnakov@uni.coventry.ac.uk>
2018-11-29 17:45:58 +0000

    Core App backend base added

---
+++ core_app/routes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const status = require('http-status-codes')
+
+router.get('/', async(req, res) => {
+    res.render('test')
+})
+
+module.exports = router
