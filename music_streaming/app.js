5b2ba6a3a80d2afee9227909afeb522d10e06e07
Oskars Pozdnakovs <opozdnakov@uni.coventry.ac.uk>
2018-12-03 14:11:37 +0000

    IMPLEMENTATION for adapting Music_Streaming song upload endpoint to receive the song file from Core_App and not Website

--- music_streaming/app.js
+++ music_streaming/app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
@@ -18,8 +17,8 @@ app.engine('hbs', hbs({
     layoutsDir: __dirname + '/../website/views/layouts/',
     partialsDir: __dirname + '/../website/views/partials/'
 }))
-app.set('views', path.join(__dirname, '../website/views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../website/views'))
+app.set('view engine', 'hbs')
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -33,39 +32,11 @@ app.use(sassMiddleware({
 }))
 app.use(express.static(path.join(__dirname, '../website/public')))
 
-app.use('/music-streaming', require('./routes'))
-
-// catch 404 and forward to error handler
-// app.use((req, res, next) => {
-// 	next(createError(404))
-// })
-
-// error handler
-// app.use((err, req, res, next) => {
-// 	// set locals, only providing error in development
-// 	res.locals.message = err.message
-// 	res.locals.error = req.app.get('env') === 'development' ? err : {}
-//
-// 	// render the error page
-// 	res.status(err.status || 500)
-// 	res.render('error')
-// })
-
-// const MongoClient = require('mongodb').MongoClient
-// const test = require('assert')
-// const url = 'mongodb://localhost:27017'
-// const dbName = 'songs'
-// // Connect using MongoClient
-// MongoClient.connect(url, (err, client) => {
-// 	// Use the admin database for the operation
-// 	const adminDb = client.db(dbName).admin()
-// 	// List all the available databases
-// 	adminDb.listDatabases((err, dbs) => {
-// 		test.equal(null, err)
-// 		test.ok(dbs.databases.length > 0)
-// 		client.close()
-// 	})
-// 	console.log('Mongo connection OK!') // TODO log
-// })
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+app.use('/', require('./routes'))
 
 module.exports = app
